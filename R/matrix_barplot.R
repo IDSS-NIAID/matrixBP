5487fcd8340b7a1573e97e88d2fc0307db50e80d
Randy Johnson <randall.johnson@nih.gov>
2025-01-23 14:53:40 -0500

    Bug fix in matrix_barplot legend colors

--- R/matrix_barplot.R
+++ R/matrix_barplot.R
@@ -91,7 +91,7 @@ matrix_barplot <- function(data = NULL, mapping = NULL,
     # default theme for matrix_barplots
     theme(legend.position = 'top') +
     
-    scale_color_gradient2(low = 'blue', high = 'red', midpoint = 'grey45')
+    scale_color_gradient2(low = 'blue', high = 'red', mid = 'grey45')
   
   
   # hide the strip text by default
